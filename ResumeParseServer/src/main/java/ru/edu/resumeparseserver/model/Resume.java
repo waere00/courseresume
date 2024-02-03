bb2d9955c40565ccf20164eba7d0e11fc5a1e29b
Valerii Eremeev <waere@yandex.ru>
2024-02-03 14:19:47 +0300

    03-02-4

--- ResumeParseServer/src/main/java/ru/edu/resumeparseserver/model/Resume.java
+++ ResumeParseServer/src/main/java/ru/edu/resumeparseserver/model/Resume.java
@@ -9,7 +9,7 @@ import jakarta.persistence.*;
 @Table(name = "resumes")
 public class Resume {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
     @Column(name = "resumeId")
